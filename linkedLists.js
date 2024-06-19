07fc4cc408e592cc26c94b1de70d10fd69fcdfb2
junaidalam2 <junaid.alam2@gmail.com>
2024-06-19 10:47:32 -0700

    completed get and set

--- linkedLists.js
+++ linkedLists.js
@@ -24,7 +24,8 @@ class SingleLinkedLists {
             this.tail = newNode;
         }
         this.length++;
-        console.log(newNode)
+        //console.log(newNode)
+        //console.log(this);
         return this;
     }
 
@@ -47,17 +48,63 @@ class SingleLinkedLists {
             }
             current = current.next;
         }
-
-
         
         const deletedNode = this.tail;
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
         return deletedNode;
-        
     }
 
+    shift() {
+        
+        if(!this.head) return;
+
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+            this.legnth = 0;
+            return;
+        }
+        
+        const currentHead = this.head;
+        this.head = this.head.next;
+        this.length--;
+        return currentHead;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+            }
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if(index < 1 || index > this.legnth) return null;
+
+        let counter = 1;
+        let current = this.head;
+        while(index <= index) {
+            if(counter == index) return current;
+            current = current.next;
+            counter++;
+        }
+    }
+
+    set(index, value) {
+        if(index < 1 || index > this.legnth) return null;
+
+        let node = this.get(index)
+        node.value = value;
+        return node;
+    }
 
 }
 
@@ -65,9 +112,16 @@ const list = new SingleLinkedLists();
 list.push(1);
 list.push(2);
 list.push(3);
+//console.log(list);
+//list.unshift(4);
 console.log(list);
-list.pop();
-console.log(list);
+//console.log(list.get(2));
+console.log(list.set(1, 11));
+
+//list.pop();
+//list.shift();
+//console.log('post-shift')
+//console.log(list);
 
 // https://blog.bitsrc.io/data-structures-in-javascript-single-linked-lists-7df02408ebbc
 
